f2b6c9a0d43923fd9d166987f71be251e2c7fed2
MANSOUL <xiaohu12347@gmail.com>
2018-07-08 22:58:47 +0800

    fix:sn

--- config/weixin.js
+++ config/weixin.js
@@ -10,8 +10,8 @@
 正式商城配置
  */
 module.exports = {
-	appid: 'wx401d956d445ec5aa',//微信支付分配的公众账号ID
-	mch_id: '1491984782',//微信支付分配的商户号
-	appSecret: '0a65a4724cbebeafd45a4806f5a08e28',
-	mch_key: 'ahgy123fvbbc089dfvalo08hnnbg5fha'
+	appid: 'wx401d9d445ec5aa',//微信支付分配的公众账号ID
+	mch_id: '14984782',//微信支付分配的商户号
+	appSecret: '0a65a4724c45a4806f5a08e28', // app secret
+	mch_key: 'ahgy123fvbbc089dfva8hnnbg5fha' // mch_key
 };
